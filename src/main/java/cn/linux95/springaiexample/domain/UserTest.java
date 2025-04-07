90dc96fdbf0f9a068ffa8be58f6e3d2c7444e396
wyl001 <281052498@qq.com>
2025-04-07 22:23:52 +0800

    feat(domain): 添加 UserTest 类

---
+++ src/main/java/cn/linux95/springaiexample/domain/UserTest.java
@@ -0,0 +1,14 @@
+package cn.linux95.springaiexample.domain;
+
+import lombok.Data;
+
+@Data
+public class UserTest {
+    private String name;
+    private Integer age;
+    private String sex;
+    private String address;
+    private String phone;
+}
+
+
